1f9c840949cc634ed2e747d7b35cb47f801daab2
Ethan Smith <ethansmith.dev@gmail.com>
2025-09-09 19:10:56 -0400

    commented out pareto mode and added some keybindings

--- init.lisp
+++ init.lisp
@@ -4,13 +4,39 @@
 
 (in-package :lem-user)
 
-(push #P"~/.lem/packages/lem-pareto/" asdf:*central-registry*)
-(asdf:load-system :lem-pareto)
-; Enable Paredit and Pareto along with Lisp mode
+; (push #P"~/.lem/packages/lem-pareto/" asdf:*central-registry*)
+; (asdf:load-system :lem-pareto)
+; ; Enable Paredit and Pareto along with Lisp mode
+; (add-hook *find-file-hook*
+;           (lambda (buffer)
+;             (when (eq (buffer-major-mode buffer)
+;                       'lem-lisp-mode:lisp-mode)
+;               (change-buffer-mode buffer 'lem-paredit-mode:paredit-mode t)
+;               (change-buffer-mode buffer 'lem-pareto-mode:pareto-mode ))))
+
+(load-theme "classic-dark")
+
 (add-hook *find-file-hook*
           (lambda (buffer)
             (when (eq (buffer-major-mode buffer)
                       'lem-lisp-mode:lisp-mode)
-              (change-buffer-mode buffer 'lem-paredit-mode:paredit-mode t)
-              (change-buffer-mode buffer 'lem-pareto-mode:pareto-mode ))))
+              (change-buffer-mode buffer 'lem-paredit-mode:paredit-mode t))))
 
+(undefine-key lem-paredit-mode:*paredit-mode-keymap* "C-h")
+
+(define-keys *global-keymap*
+  ("C-h f" 'lem-lisp-mode:lisp-describe-symbol)
+  ("C-h k" 'describe-key)
+  ("C-h m" 'describe-mode)
+  ("C-h I" 'lem-lisp-mode:lisp-inspect)
+  ("C-h b" 'describe-bindings))
+
+(define-keys lem-paredit-mode:*paredit-mode-keymap*
+  ("C-<" 'lem-paredit-mode:paredit-barf)
+  ("C->" 'lem-paredit-mode:paredit-slurp))
+
+(define-keys *global-keymap*
+  ("C-/" 'undo)
+  ("C-?" 'redo))
+
+(lem/line-numbers:toggle-line-numbers)
